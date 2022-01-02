4b52725db146eb3afb380ab6266f2ea3eb15f3db
M F M Fazrin <mfmfazrin1986@gmail.com>
2022-01-02 16:23:33 +0530

    Add project files.

---
+++ WWTravelClubREST/DTOs/PackagesListDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WWTravelClubREST.DTOs
+{
+    public record PackagesListDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int DurationInDays { get; set; }
+        public DateTime? StartValidityDate { get; set; }
+        public DateTime? EndValidityDate { get; set; }
+        public string DestinationName { get; set; }
+        public int DestinationId { get; set; }
+    }
+}
---
+++ WWTravelClubREST/Startup.cs
@@ -0,0 +1,65 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace WWTravelClubREST
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddDbContext<WWTravelClubDB.MainDBContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly("WWTravelClubDB")));
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("WWWTravelClub", new OpenApiInfo
+                {
+                    Version = "WWWTravelClub 1.0.0",
+                    Title = "WWWTravelClub",
+                    Description = "WWWTravelClub Api",
+                    TermsOfService = null
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHsts();
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint(
+                    "/swagger/WWWTravelClub/swagger.json",
+                    "WWWTravelClub Api");
+            });
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+    }
+}
